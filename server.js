34af7f40b78badd1be9de2c079cf53664d474398
Dominic Jodoin <dominic@travis-ci.com>
2017-09-27 14:55:21 +0200

    First cut of a broadcast creation API

--- server.js
+++ server.js
@@ -3,29 +3,31 @@
 
 // init project
 var express = require('express');
+var Broadcast = require('./broadcast')
+
 var app = express();
-
-
-// hi
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", function(request, response) {
+    response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
+app.get("/dreams", function(request, response) {
+    response.send(dreams);
 });
 
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/broadcasts", function (request, response) {
-  broadcast.create();
-  response.sendStatus(200);
+app.post("/broadcasts", function(request, response) {
+    if (Object.keys(request.query).length === 0) {
+        throw "Request is missing query parameters!"
+    }
+
+    var broadcast = new Broadcast();
+    broadcast.create(request.query.site, request.query.message);
+    response.sendStatus(200);
 });
 
 // Simple in-memory store for now
@@ -36,7 +38,6 @@ app.post("/broadcasts", function (request, response) {
 //];
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT, function() {
+    console.log('Your app is listening on port ' + listener.address().port);
 });
-
